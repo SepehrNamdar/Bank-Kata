d91d0082fedd89a1202cf838ec69cc39a281e67d
Sepehr NAMDAR <sepehr_gsfc@yahoo.com>
2020-06-18 11:58:45 +0200

    existing account history

--- src/test/java/model/AccountHistoryShould.java
+++ src/test/java/model/AccountHistoryShould.java
@@ -5,11 +5,14 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static java.math.BigDecimal.TEN;
 import static java.math.BigDecimal.valueOf;
 import static java.time.LocalDateTime.now;
+import static java.util.Collections.singletonList;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountHistoryShould {
@@ -28,7 +31,6 @@ public class AccountHistoryShould {
         List<Statement> expected = new ArrayList<>();
         expected.add(new Statement(TEN, new Deposit(depositAmount), now));
         expected.add(new Statement(valueOf(11.25), new Withdrawal(withdrawalAmount), now));
-
         assertThat(account.getOperationsHistory()).isEqualTo(expected);
     }
 
@@ -36,9 +38,13 @@ public class AccountHistoryShould {
     public void print_an_existing_account() {
         BigDecimal withdrawalAmount = valueOf(2.99);
         ArrayList<Statement> previousOperations = new ArrayList<>();
-        previousOperations.add(new Statement(TEN, new Withdrawal(withdrawalAmount), now));
+        Statement existingStatement = new Statement(TEN, new Withdrawal(withdrawalAmount), now);
+        previousOperations.add(existingStatement);
+
         Account account = new Account(previousOperations);
 
         assertThat(account.getBalance()).isEqualTo(valueOf(7.01));
+        List<Statement> expected = singletonList(existingStatement);
+        assertThat(account.getOperationsHistory()).isEqualTo(expected);
     }
 }
